5d65012c2162fdf0759c3ac98aaecc73444f80f4
Dinesh <paliwaldinesh116@gmail.com>
2020-10-22 12:06:29 +0530

    adding log for debug

--- gitPy.py
+++ gitPy.py
@@ -1,7 +1,7 @@
 import subprocess
 log=subprocess.run(["git", "log"],capture_output=True)
 log_readable=log.stdout.decode()
-print("This is log")
+print("This is log for debugging")
 print(log_readable)
 file = open("delta.txt", "w")
 file.write(log_readable)
